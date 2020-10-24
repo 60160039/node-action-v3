3406d1bac5a76d9906a5d9a39b7a09038a8b98a2
60160039 <60160039@go.buu.ac.th>
2020-10-24 16:12:11 +0700

    lastest

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
-const assert = require('supertest')
-const assert = require('../index')
+const request = require('supertest')
+const app = require('../index')
 
 describe('Validate setting the test', () => {
     it('should be ok', () => {
@@ -11,5 +11,15 @@ describe('Validate setting the test', () => {
 describe('GET "/"', () => {
     it('respond with Hello!', (done) => {
         request(app).get('/').expect('Hello!', done)
+    })
+})
+
+describe('GET "/item"', () => {
+    it('respond with array of item', (done) => {
+        let want = [
+            { id: 1, name:'iphone 12' }
+            { id: 1, name:'Nokia 3310' }
+        ]
+        request(app).get('/items').expect('want', done)
     })
 })
